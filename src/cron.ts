1804de2c9c39f9f8d14c5df7a552411fbcfb76b5
ShaunLWM <shaunlimweimin@gmail.com>
2022-09-08 21:56:06 +0800

    Add SearchTerm models

--- src/cron.ts
+++ src/cron.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import { SGCarMart } from "sgcarmart.js";
 import CarHistory from "./models/CarHistory";
 import TrackedCar from "./models/TrackedCar";
+import SearchTerm from "./models/SearchTerm";
 
 interface DiffObject {
   added: Record<string, any>;
@@ -12,70 +13,78 @@ interface DiffObject {
   updated: Record<string, any>;
 }
 
-const SEARCH_TERMS = "Mazda 3 Mild Hybrid";
-
 const client = new SGCarMart();
 
 async function onScrape() {
   let page = 1;
 
-  while (true) {
-    console.log(`[scrape] page ${page}`);
-    const results = await client.getLatestUsed({ search: SEARCH_TERMS, page });
-    if (results.length < 1) {
-      console.log(`[cron] no results found on page ${page}`);
-      break;
-    }
+  const searchTerms = await SearchTerm.find();
+  if (searchTerms.length < 1) {
+    console.log("[cron] no search terms found");
+    process.exit(0);
+  }
 
-    const carIds = results.map(result => result.id);
-    console.log(`[cron] scraped ${carIds.length} cars`);
-
-    const [carInfos, existingCars] = await Promise.all([
-      Promise.allSettled(carIds.map(id => client.getCarInfo(id))),
-      TrackedCar.find({ carId: { $in: carIds } }).select(["carId", "data"]).exec(),
-    ]);
-
-    const successfulCarInfos = (carInfos.filter(info => info.status === "fulfilled") as PromiseFulfilledResult<any>[]).map(info => info.value);
-    console.log(`[cron] ${successfulCarInfos.length} scraped cars. ${existingCars.length} existing cars.`);
-
-    for (const existingCar of existingCars) {
-      // for each of the existing cars, check if the scraped data is successful
-      const carIndex = successfulCarInfos.findIndex(info => info.id === existingCar.carId);
-      if (carIndex < 0) {
-        continue;
+  for (const searchTerm of searchTerms) {
+    const { term } = searchTerm;
+    console.log(`[cron] scraping ${term}`);
+    while (true) {
+      console.log(`[cron] page ${page}`);
+      const results = await client.getLatestUsed({ search: term, page });
+      if (results.length < 1) {
+        console.log(`[cron] no results found on page ${page}`);
+        break;
       }
 
-      const carInfo = successfulCarInfos[carIndex];
-      // check the diff and check if the diff has keys
-      const diff = detailedDiff(JSON.parse(existingCar.data), carInfo) as DiffObject;
-      if (Object.keys(diff["added"]).length < 1 && Object.keys(diff["deleted"]).length < 1 && Object.keys(diff["updated"]).length < 1) {
-        successfulCarInfos.splice(carIndex, 1);
-        continue;
-      }
+      const carIds = results.map(result => result.id);
+      console.log(`[cron] scraped ${carIds.length} cars`);
 
-      console.log(`[cron] ${existingCar.carId} - changes detected`);
-      existingCar.data = JSON.stringify(carInfo);
-      await Promise.allSettled([
-        new CarHistory({ carId: existingCar.carId, from: existingCar.data, to: JSON.stringify(carInfo), lastUpdated: new Date() }).save(),
-        existingCar.save()
+      const [carInfos, existingCars] = await Promise.all([
+        Promise.allSettled(carIds.map(id => client.getCarInfo(id))),
+        TrackedCar.find({ carId: { $in: carIds } }).select(["carId", "data"]).exec(),
       ]);
 
-      successfulCarInfos.splice(carIndex, 1);
-    }
+      const successfulCarInfos = (carInfos.filter(info => info.status === "fulfilled") as PromiseFulfilledResult<any>[]).map(info => info.value);
+      console.log(`[cron] ${successfulCarInfos.length} scraped cars. ${existingCars.length} existing cars.`);
 
-    if (successfulCarInfos.length > 0 || existingCars.length < 1) {
-      const newCars = successfulCarInfos.map(info => new TrackedCar({ carId: info.id, name: info.name, data: JSON.stringify(info), tag: SEARCH_TERMS }));
-      const results = await TrackedCar.insertMany(newCars);
-      console.log(`[cron] inserted ${results.length} new cars`);
-    }
+      for (const existingCar of existingCars) {
+        // for each of the existing cars, check if the scraped data is successful
+        const carIndex = successfulCarInfos.findIndex(info => info.id === existingCar.carId);
+        if (carIndex < 0) {
+          continue;
+        }
 
-    page += 1;
-    if (page === 20 || results.length < 20) {
-      console.error("[limit] reached page 20 or current page < 20, stopping..");
-      break;
-    }
+        const carInfo = successfulCarInfos[carIndex];
+        // check the diff and check if the diff has keys
+        const diff = detailedDiff(JSON.parse(existingCar.data), carInfo) as DiffObject;
+        if (Object.keys(diff["added"]).length < 1 && Object.keys(diff["deleted"]).length < 1 && Object.keys(diff["updated"]).length < 1) {
+          successfulCarInfos.splice(carIndex, 1);
+          continue;
+        }
 
-    console.log(`------------------`);
+        console.log(`[cron] ${existingCar.carId} - changes detected`);
+        existingCar.data = JSON.stringify(carInfo);
+        await Promise.allSettled([
+          new CarHistory({ carId: existingCar.carId, from: existingCar.data, to: JSON.stringify(carInfo), lastUpdated: new Date() }).save(),
+          existingCar.save()
+        ]);
+
+        successfulCarInfos.splice(carIndex, 1);
+      }
+
+      if (successfulCarInfos.length > 0 || existingCars.length < 1) {
+        const newCars = successfulCarInfos.map(info => new TrackedCar({ carId: info.id, name: info.name, data: JSON.stringify(info), tag: term }));
+        const results = await TrackedCar.insertMany(newCars);
+        console.log(`[cron] inserted ${results.length} new cars`);
+      }
+
+      page += 1;
+      if (page === 20 || results.length < 20) {
+        console.error("[limit] reached page 20 or current page < 20, stopping..");
+        break;
+      }
+
+      console.log(`------------------`);
+    }
   }
 
   process.exit(0);
---
+++ src/models/SearchTerm.ts
@@ -0,0 +1,28 @@
+import { Document, Model, model, Schema } from "mongoose";
+
+export interface ISearchTerm {
+  term: string;
+}
+
+const ISearchTermSchemaFields: Record<keyof ISearchTerm, any> = {
+  term: {
+    type: String,
+    required: true,
+  },
+};
+
+const ISearchTermSchema = new Schema<ISearchTermDocument, ISearchTermModel>(ISearchTermSchemaFields, {
+  timestamps: false,
+});
+
+export interface ISearchTermDocument extends ISearchTerm, Document { }
+
+ISearchTermSchema.statics.getUniqueLicensePlatesCount = async function (): Promise<number> {
+  return await this.distinct("carId").count().exec();
+};
+
+export interface ISearchTermModel extends Model<ISearchTermDocument> {
+  getUniqueLicensePlatesCount(): Promise<number>;
+}
+
+export default model<ISearchTermDocument, ISearchTermModel>("SearchTerm", ISearchTermSchema);
